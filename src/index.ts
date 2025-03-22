7c8669074afca61f084e43e3d6a7d167c29037d8
Stefan Rinke <sker65@gmail.com>
2025-03-22 15:00:43 +0100

    executable / version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { checkNotifications, registerTools } from "./tools";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
