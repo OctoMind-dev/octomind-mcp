25b81ff8c57b9a5d70bb0fe2fcf0acd4a661593c
Stefan Rinke <sker65@gmail.com>
2025-03-31 19:16:20 +0200

    logger binding

--- src/logger.ts
+++ src/logger.ts
@@ -1,23 +1,25 @@
 import { Logger, pino } from "pino";
 
-let logger: Logger;
+const buildLogger = (): Logger => {
+  if (process.env.LOG_FILENAME) {
+    // Create a logger that writes to the specified file
+    return pino(
+      {
+        level: process.env.LOG_LEVEL || "info",
+      },
+      pino.destination({
+        dest: process.env.LOG_FILENAME,
+        sync: false,
+      }),
+    );
+  } else {
+    // Create a "silent" logger that doesn't output anywhere
+    return pino({
+      enabled: false,
+    });
+  }
+};
 
-if (process.env.LOG_FILENAME) {
-  // Create a logger that writes to the specified file
-  logger = pino(
-    {
-      level: process.env.LOG_LEVEL || "info",
-    },
-    pino.destination({
-      dest: process.env.LOG_FILENAME,
-      sync: false,
-    }),
-  );
-} else {
-  // Create a "silent" logger that doesn't output anywhere
-  logger = pino({
-    enabled: false,
-  });
-}
+const logger = buildLogger();
 
 export { logger };
