20f6c7b300b12a5b0b6c1eabc4a41ad40fd10988
Stefan Rinke <sker65@gmail.com>
2025-05-06 15:37:58 +0200

    revert hints

--- src/handlers.ts
+++ src/handlers.ts
@@ -181,13 +181,6 @@ export function registerDiscoveryTool(
           "Optional folder name that the newly discovered test case will be added to",
         ),
     },
-    {
-      title: "Discover a test case",
-      readOnlyHint: false,
-      destructiveHint: true,
-      idempotentHint: false,
-      openWorldHint: true,
-    },
     async (params) => {
       return await handler.execute(params);
     },
