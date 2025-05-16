afd9bdbb3f171d66e383dfa98140888548cab0ec
Stefan Rinke <sker65@gmail.com>
2025-05-16 14:27:56 +0200

    alias support

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,9 @@ const jestConfig: jest.Config = {
   testTimeout: 20_000,
   watchPathIgnorePatterns: ["temp"],
   prettierPath: null, //https://github.com/jestjs/jest/issues/14305 -> jest 30 will support it
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
 };
 
 // noinspection JSUnusedGlobalSymbols
